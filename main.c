8b4412af301e81720172aedccd0f54151bb5e45e
Anant Joshi <Anant Joshi>
2016-12-19 14:21:51 +0530

    Add code to move to next node

--- main.c
+++ main.c
@@ -23,6 +23,10 @@
 #include "mnp_positions.h"
 #include "motor_movement.h"
 
+int flag_to_restart = 0;
+int curr_node_pos = 0, prev_node_pos 23, next_node_pos;
+int num_turn_paths;
+
 /*
 	Start Definitions for buzzer
 */
@@ -87,83 +91,42 @@ void port_init (void)
 /*
 	Initialize devices
 */
+
+
+void init_graph(void){
+	init_grid_of_nodes();
+	add_both_nodeinfo();
+}
 void init_devices (void)
 {
 	cli(); //Clears the global interrupts
 	port_init();
+	adc_init();
+	init_graph();
+	init_mnp();
 	sei(); //Enables the global interrupts
 }
 
-
-/*
-	Function Name:	is_a_node
-	Input:			No parameters, ADC Input from white line sensors
-	Output:			Returns 1 if the current position is a node, zero otherwise
-	Logic:			If the central, and either of the left or right white line sensors
-					give a false value, then the current position is a node
-	Example Call:	is_a_node() (Generally in a conditional statement)	
-*/
-
-char is_a_node(){
-	left_white_line = ADC_Conversion(3);
-	centre_white_line = ADC_Conversion(2);
-	right_white_line = ADC_Conversion(1);
-
-	if(centre_white_line > 0x28 && (right_white_line > 0x28 || left_white_line > 0x28)){
-		return 1;
+void move_to(int j){
+	if(sp_parent[j] == -1 || flag_to_restart == 1){
+		return
 	}else{
-		return 0;
-	}
-}
-
-/*
-	Function Name:	follow_line
-	Input:			No parameters, ADC input from the White line sensors
-	Output: 		None. (Moves motors)
-	Logic:			If the central white line sensor is high, then the robot is on the black line
-					if the left sensor is high, then the robot must curve left
-					if the right sensor is high, then the robot must curve right
-	Example Call:	follow_line() (It is just an imperative statement)
-*/
-void follow_line(){
-	Left_white_line = ADC_Conversion(3);	//Getting data of Left WL Sensor
-	Center_white_line = ADC_Conversion(2);	//Getting data of Center WL Sensor
-	Right_white_line = ADC_Conversion(1);	//Getting data of Right WL Sensor				
-
-	if(Center_white_line > 0x28)
-	{
-			forward();
-			velocity(150,150);
-	}
-
-		if((Left_white_line > 0x28))
-		{
-			forward();
-			velocity(130,50);
+		move_to(sp_parent[j]);
+		num_turn_paths = path_number(curr_node_pos, prev_node_pos, next_node_pos);
+		turn_to_path(num_paths);
+		if(!handle_obstacle(ADC_Conversion(6), curr_node_pos, next_node_pos, num_turn_paths)){
+			follow_black_line();
+			flag_to_restart = 0;
+		}else{
+			flag_to_restart = 1;
+			return;
 		}
-
-		if((Right_white_line > 0x28))
-		{
-			forward();
-			velocity(50,130);
-		}
-
-		// TODO: Check later
-		// if(Center_white_line>0x28 && Left_white_line>0x28 && Right_white_line>0x28)
-		// {
-		// 	forward();
-		// 	velocity(0,0);
-		// }
+	}
 }
 
 
 int main(void)
 {
-    while (1){
-		buzzer_on()
-		_delay_ms(1000);
-		buzzer_off();
-		_delay_ms(1000);
-    }
+    init_devices();
 }
 
