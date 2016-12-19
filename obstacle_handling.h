a54ea6063e5da2007ced39217193dd4b602c7510
Anant Joshi <anantjoshi1324@gmail.com>
2016-12-19 16:00:35 +0530

    Add code for turning back, also rename function

--- obstacle_handling.h
+++ obstacle_handling.h
@@ -4,18 +4,18 @@
 #include "dijkstras_algo.h"
 #include "bothoven_graph.h"
 #include "motor_movement.h"
+#include "follow_line.h"
 #include <stdlib.h>
 #define PROX_THRESHOLD 30
 
-char check(int prox, int prev, int next){    //update global next
+char handle_obstacle(int prox, int prev, int next, int num_paths){    //update global next
 	if(prox>PROX_THRESHOLD){
-		remove_egde(prev, next)
+		remove_edge(prev, next)
 		dijkstra(grid_of_nodes, prev);
+		turn_to_path(-num_paths);
 		return 1;
 	}
 	return 0;
 }
-
-
 
 #endif
